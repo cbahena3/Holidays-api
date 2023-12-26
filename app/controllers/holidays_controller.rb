974133800ab9275419472d5fc1de5dfede27c4b8
stwardak <148924249+stwardak@users.noreply.github.com>
2023-12-26 14:19:03 -0600

    added create action

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -3,4 +3,15 @@ class HolidaysController < ApplicationController
     @holidays = Holiday.all
     render :index
   end
+
+  def create
+    @holiday = Holiday.create(
+      name: params[:name],
+      date: params[:date],
+      image: params[:image],
+      description: params[:description],
+    )
+    render :show 
+  end
+
 end
--- test/controllers/holidays_controller_test.rb
+++ test/controllers/holidays_controller_test.rb
@@ -8,4 +8,12 @@ class HolidaysControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Holiday.count, data.length
   end
+
+  test "create" do
+    assert_difference "Holiday.count", 1 do
+      post "/holidays.json", params: { name: "Halloween", date: "October 31", image: "https://assets.editorial.aetnd.com/uploads/2009/11/halloween-gettyimages-1424736925.jpg" ,description: "Lots of candy!!!!" }
+      assert_response 200
+    end
+  end
+
 end
